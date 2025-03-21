d7d13d67e9c779fe28aef6d8488cf6805f675158
DavidGhanem <davidghanem0@gmail.com>
2025-03-21 22:02:14 +0200

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,19 @@
+let darkmode = localStorage.getItem('darkmode'); 
+const themeSwitch = document.getElementById('theme-switch');
+
+if (darkmode === 'active') enableDarkMode(); // Apply dark mode if stored
+
+themeSwitch.addEventListener('click', () => {
+    darkmode = localStorage.getItem('darkmode'); // Fetch current state
+    darkmode === 'active' ? disableDarkMode() : enableDarkMode(); // Toggle mode
+});
+
+function enableDarkMode() {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmode', 'active'); // Store dark mode state
+}
+
+function disableDarkMode() {
+    document.body.classList.remove('darkmode');
+    localStorage.removeItem('darkmode'); // Remove from localStorage
+}
